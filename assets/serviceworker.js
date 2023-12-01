9da38e5e68840f91e185be81436d9c55a9386c5f
afonsosousah <afonsosousah@hotmail.com>
2023-12-01 19:24:08 +0000

    v0.0.1 - Initial commit

---
+++ assets/serviceworker.js
@@ -0,0 +1,8 @@
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+    console.log("Service worker installed");
+ });
+ self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+ });
+ 
