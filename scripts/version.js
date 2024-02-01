01c52ec334e9d2838e1383ed2beef287f7d04dfc
afonsosousah <afonsosousah@hotmail.com>
2024-02-01 13:28:05 +0000

    update: added update notes and change version in code

--- scripts/version.js
+++ scripts/version.js
@@ -1,37 +1,34 @@
-
-
 // Current version number
-const currentVersion = "0.0.3";
+const currentVersion = "0.0.4";
 
 // Changelog HTML
 const changelogHTML = `
-Nova versão 0.0.3!<br>
+Nova versão 0.0.4!<br>
 <ul>
-    <li>Mudança da UI</li>
-    <li>Suporte para botão voltar atrás nativo (obrigado DanielAgostinho)</li>
-    <li>Pedidos e error handling melhorados (obrigado rodrigoleitao)</li>
-    <li>Proxy já não é utilizado no login na API da EMEL (obrigado j0dd)</li>
-    <li>Melhorias no sistema de navegação (já não utiliza a bússola do dispositivo)</li>
+    <li>Novo menu de estatísticas</li>
+    <li>Novo menu de histórico de viagens</li>
+    <li>Navegação em 3D e bastante melhorada</li>
+    <li>Ciclovias mostradas no mapa (obrigado Francisco Zhuo)</li>
+    <li>Melhorias gerais no código (obrigado Rodrigo Leitão)</li>
 </ul>
 `;
 
-
 function showUpdateInfoIfNeeded() {
-    setTimeout(() => {
-        // Set the cookie expiry to 1 year after today.
-        const expiryDate = new Date();
-        expiryDate.setFullYear(expiryDate.getFullYear() + 1);
+	setTimeout(() => {
+		// Set the cookie expiry to 1 year after today.
+		const expiryDate = new Date();
+		expiryDate.setFullYear(expiryDate.getFullYear() + 1);
 
-        // Check version to show update notes
-        const userVersion = getCookie("version");
-        if (userVersion) {
-            if (userVersion !== "0.0.3") {
-                alert(changelogHTML);
-                document.cookie = "version=" + currentVersion + "; expires=" + expiryDate.toGMTString();
-            }
-        } else {
-            alert(changelogHTML);
-            document.cookie = "version=" + currentVersion + "; expires=" + expiryDate.toGMTString();
-        }
-    }, 1500);
+		// Check version to show update notes
+		const userVersion = getCookie("version");
+		if (userVersion) {
+			if (userVersion !== currentVersion) {
+				alert(changelogHTML);
+				document.cookie = "version=" + currentVersion + "; expires=" + expiryDate.toGMTString();
+			}
+		} else {
+			alert(changelogHTML);
+			document.cookie = "version=" + currentVersion + "; expires=" + expiryDate.toGMTString();
+		}
+	}, 1500);
 }
