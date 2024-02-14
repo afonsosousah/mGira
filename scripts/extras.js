d03946ba1c7f009b3380e834c4d6b0350d16ae90
Afonso Hermenegildo <62546134+afonsosousah@users.noreply.github.com>
2024-02-14 20:28:24 +0000

    fix: Apply suggestions from code review
    Co-authored-by: Rodrigo Leitão <38259440+ImRodry@users.noreply.github.com>

--- scripts/extras.js
+++ scripts/extras.js
@@ -95,7 +95,7 @@ function parseMillisecondsIntoTripTime(milliseconds, showSeconds = true) {
 }
 
 function downloadObjectAsJson(exportObj, exportName) {
-	const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+	const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, "\t"));
 	const downloadAnchorNode = document.createElement("a");
 	downloadAnchorNode.setAttribute("href", dataStr);
 	downloadAnchorNode.setAttribute("download", exportName + ".json");
