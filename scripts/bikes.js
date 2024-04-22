05ab78a223f5b51799981de1e74edfd981082f3e
afonsosousah <afonsosousah@hotmail.com>
2024-04-22 22:00:20 +0100

    Update bikes.js

--- scripts/bikes.js
+++ scripts/bikes.js
@@ -4,6 +4,8 @@ let ratedTripsList = [];
 let finishedTripsList = [];
 let tripBeingRated = false;
 
+//test
+
 // reserves the bike and returns a success boolean
 async function reserveBike(serialNumber) {
 	const response = await makePostRequest(
