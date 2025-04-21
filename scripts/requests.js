880735aafa6b3a6d61328cc0094db2edb9843406
Rodry <38259440+ImRodry@users.noreply.github.com>
2025-04-21 01:41:49 +0100

    feat: use ws url

--- scripts/requests.js
+++ scripts/requests.js
@@ -3,8 +3,9 @@ let proxyURL = null;
 let activeTripObj;
 let currentRequestTry = 0;
 
-const GIRA_GRAPHQL_ENDPOINT = "https://c2g091p01.emel.pt/api/graphql";
+// const GIRA_GRAPHQL_ENDPOINT = "https://c2g091p01.emel.pt/api/graphql";
 const GIRA_GRAPHQL_WS_ENDPOINT = "wss://c2g091p01.emel.pt/ws/graphql";
+const GIRA_GRAPHQL_ENDPOINT = GIRA_GRAPHQL_WS_ENDPOINT.replace("wss://", "https://");
 const GIRA_AUTH_ENDPOINT = "https://api-auth.emel.pt/auth";
 const GIRA_TOKEN_REFRESH_ENDPOINT = "https://api-auth.emel.pt/token/refresh";
 const GIRA_USER_ENDPOINT = "https://api-auth.emel.pt/user";
